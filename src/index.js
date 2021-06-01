9e87d43ce5f828cf36f0a18794045b4a5d78f33c
Oyeleke Ayomide <haywhizzz@gmail.com>
2021-06-01 05:34:16 -0700

    Update Index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 import store from './store';
 
 ReactDOM.render(
