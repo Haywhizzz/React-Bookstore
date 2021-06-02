4075d7b8eb4b48df911d730ae7008d5f37517537
Oyeleke Ayomide <haywhizzz@gmail.com>
2021-06-02 11:07:07 +0100

    Add some styling to the page

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 function Book({
   id, title, category, deleteBook,
 }) {
+  const percentage = 66;
   return (
-    <tr>
-      <td>{id}</td>
-      <td>{title}</td>
-      <td>{category}</td>
-      <td><button onClick={() => deleteBook(id)} type="button">Delete</button></td>
-    </tr>
+    <div className="books d-flex justify-content-between p-3 m-5">
+      <div className="m-5 pr-5">
+        <h6 className="text-muted">{category}</h6>
+        <h4 className="">{title}</h4>
+        <div>
+
+          <button className="options" type="button">Comments</button>
+          |
+          <button onClick={() => deleteBook(id)} className="options" type="button">Remove</button>
+          |
+          <button className="options" type="button">Edit</button>
+        </div>
+
+      </div>
+      <div className="d-flex">
+        <div className="percent w-25">
+          <CircularProgressbar value={percentage} text={`${percentage}%`} />
+        </div>
+        <div className="p-3">
+          <p>CURRENT CHAPTER</p>
+          <p>Chapter 17</p>
+          <button className="btn btn-primary px-3" type="button">Update Progress</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
